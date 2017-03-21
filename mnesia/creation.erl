9c94346b4d122343a6548c0abcb0cfeec588d86c
Renato Carauta Ribeiro <rcarauta@unb.br>
2017-03-21 12:17:55 -0300

    inserção em mnesia funcional. Consulta com erro.

--- mnesia/creation.erl
+++ mnesia/creation.erl
@@ -9,19 +9,35 @@
 %% ====================================================================
 %% API functions
 %% ====================================================================
--export([install/1]).
+-export([install/1,insert/0, read/1]).
 
 install(Nodes) ->
 	ok = mnesia:create_schema(Nodes),
-	application:start(mnesia),
+	mnesia:start(),
 	mnesia:create_table(recipe,
 						[{attributes, record_info(fields,
 												  recipe)},
 						 {index,[#recipe.id]},
 						 {disc_copies, Nodes}]),
-	io:format("Schema formed!!!!!"),
-	application:stop(mnesia).
+	
+	io:format("Schema formed!!!!!").
 
+
+insert() ->
+	AdRecipe = #recipe{id = 1, name = "Teste",instruction="Instrucao"},
+	Write = fun() ->
+		mnesia:write(AdRecipe)
+	end,
+	mnesia:transaction(Write),
+	io:format("data insert on table").
+
+read(Id) ->
+
+	Read = fun() ->
+		mnesia:read(recipe, Id)
+	end,
+	
+	mnesia:transaction(Read).
 
 
 %% ====================================================================
